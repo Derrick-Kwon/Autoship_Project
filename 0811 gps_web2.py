dc9f217dbeed4fe27d3445d0366550e84a03f662
권형진 <f5863@naver.com>
2023-09-06 17:42:26 +0900

    Delete 0811 gps_web2.py

--- 0811 gps_web2.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('map_with_geolocation_osm.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
